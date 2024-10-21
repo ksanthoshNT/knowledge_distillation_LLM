73cce2c409b1828c761aba4109303a1b932a683e
kammarisanthosh <kammari.santhosh@newgensoft.com>
2024-10-21 18:16:58 +0530

    set batch size default 16

--- knowledge_distillation/src/main/final.py
+++ knowledge_distillation/src/main/final.py
@@ -145,7 +145,9 @@ class KnowledgeDistillation:
         logger.info("Loading teacher model...")
         self.teacher_model = AutoModelForCausalLM.from_pretrained(args.teacher_model_name,
                                                                   device_map="auto",
-                                                                  torch_dtype=torch.bfloat16)
+                                                                  torch_dtype=torch.bfloat16,
+                                                                  token = "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"
+                                                                  )
         self.teacher_model.config.pad_token_id = self.tokenizer.pad_token_id
         # self.teacher_model.gradient_checkpointing_enable()
         self.teacher_model.eval()
@@ -154,7 +156,9 @@ class KnowledgeDistillation:
         logger.info("Loading student model...")
         self.student_model = AutoModelForCausalLM.from_pretrained(args.student_model_name,
                                                                   device_map="auto",
-                                                                  torch_dtype=torch.bfloat16)
+                                                                  torch_dtype=torch.bfloat16,
+                                                                  token = "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"
+                                                                  )
         self.student_model.config.pad_token_id = self.tokenizer.pad_token_id
         self.student_model.gradient_checkpointing_enable()
         logger.info("Gradient checkpointing enabled for student model")
