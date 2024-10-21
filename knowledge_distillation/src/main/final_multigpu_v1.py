3d868340fc994fdff232e998b5b4a66a15d5f849
kammarisanthosh <kammari.santhosh@newgensoft.com>
2024-10-21 19:47:15 +0530

    set batch size default 16

--- knowledge_distillation/src/main/final_multigpu_v1.py
+++ knowledge_distillation/src/main/final_multigpu_v1.py
@@ -166,6 +166,9 @@ def main():
 
     torch.manual_seed(args.seed)
     tokenizer = AutoTokenizer.from_pretrained(args.teacher_model_name)
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.pad_token_id = tokenizer.eos_token_id
 
     train_dataset = LMDataset(args, tokenizer, "train")
 
