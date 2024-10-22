b378b7e14e03aca41aaf4fc5d847eebd7d17b41b
kammarisanthosh <kammari.santhosh@newgensoft.com>
2024-10-22 15:59:06 +0530

    fixed config issues

--- knowledge_distillation/src/main/evaluation/batched_main.py
+++ knowledge_distillation/src/main/evaluation/batched_main.py
@@ -7,6 +7,7 @@ def main():
     # 1. Using config.ini file
     dir_path = Path(__file__).parent
     config_path =  str( dir_path / "knowledge_distillation/src/main/evaluation/config.ini")
+    print(config_path)
     evaluator1 = TransformerEvaluator(config_path)
     evaluator1.evaluate(split_name='validation')
 
