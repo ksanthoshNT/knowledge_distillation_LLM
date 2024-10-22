e3c58f2bd9f18e1294f0b5b0482b74bb2dafb7b5
kammarisanthosh <kammari.santhosh@newgensoft.com>
2024-10-22 15:59:58 +0530

    fixed config issues

--- knowledge_distillation/src/main/evaluation/batched_main.py
+++ knowledge_distillation/src/main/evaluation/batched_main.py
@@ -5,8 +5,7 @@ from knowledge_distillation.src.main.evaluation.batched_evaluator import Transfo
 
 def main():
     # 1. Using config.ini file
-    dir_path = Path(__file__).parent
-    config_path =  str( dir_path / "knowledge_distillation/src/main/evaluation/config.ini")
+    config_path =  "knowledge_distillation/src/main/evaluation/config.ini"
     print(config_path)
     evaluator1 = TransformerEvaluator(config_path)
     evaluator1.evaluate(split_name='validation')
--- knowledge_distillation/src/main/evaluation/main.py
+++ knowledge_distillation/src/main/evaluation/main.py
@@ -6,8 +6,8 @@ from knowledge_distillation.src.main.evaluation.evaluator import TransformerEval
 
 def main():
     # 1. Using config.ini file
-    dir_path = Path(__file__).parent
-    config_path =  str( dir_path / "knowledge_distillation/src/main/evaluation/config.ini")
+    config_path = "knowledge_distillation/src/main/evaluation/config.ini"
+    print(config_path)
     evaluator1 = TransformerEvaluator(config_path)
     evaluator1.evaluate(split_name='validation')
     exit()
