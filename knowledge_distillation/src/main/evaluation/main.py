bf9d21340a26c8811475b45c35354060eba7ea50
kammarisanthosh <kammari.santhosh@newgensoft.com>
2024-10-22 15:57:58 +0530

    fixed config issues

--- knowledge_distillation/src/main/evaluation/main.py
+++ knowledge_distillation/src/main/evaluation/main.py
@@ -7,7 +7,7 @@ from knowledge_distillation.src.main.evaluation.evaluator import TransformerEval
 def main():
     # 1. Using config.ini file
     dir_path = Path(__file__).parent
-    config_path =  str( dir_path / "config.ini")
+    config_path =  str( dir_path / "src/main/evaluation/config.ini")
     evaluator1 = TransformerEvaluator(config_path)
     evaluator1.evaluate(split_name='validation')
     exit()
