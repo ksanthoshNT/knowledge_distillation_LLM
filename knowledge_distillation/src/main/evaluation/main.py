64a881eab298b535d951950e6ecf9a4a257c21ea
kammarisanthosh <kammari.santhosh@newgensoft.com>
2024-10-22 12:29:50 +0530

    add evaluation

--- knowledge_distillation/src/main/evaluation/main.py
+++ knowledge_distillation/src/main/evaluation/main.py
@@ -4,7 +4,7 @@ from knowledge_distillation.src.main.evaluation.evaluator import TransformerEval
 
 def main():
     # 1. Using config.ini file
-    evaluator1 = TransformerEvaluator("config.ini")
+    evaluator1 = TransformerEvaluator("./config.ini")
     evaluator1.evaluate(split_name='validation')
     exit()
 
