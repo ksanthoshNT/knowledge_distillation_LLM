eefc90b89acd49f1e2aa39cb05599672ad5a9438
kammarisanthosh <kammari.santhosh@newgensoft.com>
2024-10-28 18:17:04 +0530

    udpated gpu evaluation script

--- infer_distilled_model.py
+++ infer_distilled_model.py
@@ -2,12 +2,12 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # Load model and tokenizer
 model_path: str = "/home/data_science/project_files/santhosh/knowledge_distillation_LLM/knowledge_distillation/src/main/distillation/llama3-8b-awq-distilled-f32"
-model = AutoModelForCausalLM.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(model_path).cuda()
 tokenizer = AutoTokenizer.from_pretrained("aspenita/llama-3-sqlcoder-8b-AWQ")
 
 # Generate text
 prompt = "who are you"
-inputs = tokenizer(prompt, return_tensors="pt")
+inputs = tokenizer(prompt, return_tensors="pt").to('cuda')
 outputs = model.generate(**inputs, max_new_tokens=5)
 result = tokenizer.decode(outputs[0], skip_special_tokens=True)
 print(result)
